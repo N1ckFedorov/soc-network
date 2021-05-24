dcd3107b387d5bee8226f381ede14cae296b4006
Nick <nick.fedorov.root@gmail.com>
2021-05-24 15:15:38 +0300

    start project

---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src="https://www.istockphoto.com/resources/images/HomePage/Hero/682374404.jpg" alt="" />
+      </div>
+      <div>
+        <img src="https://sun9-51.userapi.com/impf/c840437/v840437709/1e44b/Y5Ln9NPuKR8.jpg?size=800x800&quality=96&sign=124f4c2dec04aeeb4d8103aebcbb732d&type=album" alt="" />
+          ava + desc
+        </div>
+      <div>
+        my post
+          <div>
+          new post
+          </div>
+        <div>
+          <div>
+            post 1
+            </div>
+          <div>
+            post 2
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
